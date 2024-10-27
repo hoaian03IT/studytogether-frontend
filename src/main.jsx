fc173b25b9750c8b97639ca62157d2d47f84da59
hoaian03IT <dangtranhoaian8603@gmail.com>
2024-10-27 17:28:29 +0700

    update package

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "draft-js/dist/Draft.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { BrowserRouter } from "react-router-dom";
 import "./config/i18next.js";
